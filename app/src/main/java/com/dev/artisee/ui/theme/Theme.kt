fca36c7e8a6bf8e91d4b14a6e9fe216547ba0210
dayjeong <dayjeong@gmail.com>
2023-10-22 15:26:20 +0900

    [FEAT] add colors (now in android colors)

--- app/src/main/java/com/dev/artisee/ui/theme/Theme.kt
+++ app/src/main/java/com/dev/artisee/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.dev.artisee.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,39 +8,29 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
+import com.dev.artisee.ui.theme.color.*
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+val LocalColors = staticCompositionLocalOf { ArtiseeLightColorScheme }
+
+val ArtiseeDarkColorScheme = darkColorScheme(
+    primary = DarkDefaultColorScheme.primary,
+    secondary = DarkDefaultColorScheme.secondary,
+    tertiary = DarkDefaultColorScheme.tertiary
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+val ArtiseeLightColorScheme = lightColorScheme(
+    primary = LightDefaultColorScheme.primary,
+    secondary = LightDefaultColorScheme.secondary,
+    tertiary = LightDefaultColorScheme.tertiary
 )
 
 @Composable
 fun ArtiseeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = true, // Dynamic color is available on Android 12+
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -50,21 +39,21 @@ fun ArtiseeTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
+        darkTheme -> DarkAndroidColorScheme
+        else -> LightAndroidColorScheme
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    val artiseeColorScheme = when {
+        darkTheme -> ArtiseeDarkColorScheme
+        else -> ArtiseeLightColorScheme
+    }
+
+    CompositionLocalProvider(LocalColors provides artiseeColorScheme) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
+
 }
