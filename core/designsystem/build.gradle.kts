d6ac5afc2264032a7b017fdd8c0452108270aa84
dayjeong <dayjeong@gmail.com>
2023-11-04 23:13:44 +0900

    [FEAT] core:designsystem gradle setting

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,69 +1,38 @@
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.dev.designsystem"
-    compileSdk = 33
-
+    compileSdk = com.dev.artisee.Configuration.compileSdk
     defaultConfig {
-        applicationId = "com.dev.designsystem"
-        minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = com.dev.artisee.Versions.jvmTarget
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = com.dev.artisee.Versions.kotlinCompilerExtensionVersion
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(platform(com.dev.artisee.Libraries.kotlin_bom))
+    implementation(com.dev.artisee.Libraries.androidx_core)
+
+    // Compose
+    implementation(com.dev.artisee.Libraries.compose_ui)
+    implementation(com.dev.artisee.Libraries.ui_tool_preview)
+    implementation(com.dev.artisee.Libraries.compose_material)
+    implementation(com.dev.artisee.Libraries.compose_material3)
+    implementation(com.dev.artisee.Libraries.activity_compose)
 }
